cd98b880990abf452c991ef0669d56823b1627f3
DurgaPydahSoft <durgaprasad@pydahsoft.in>
2026-04-20 14:14:43 +0530

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -41,15 +41,15 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://pydah.edu.in https://*.pydah.edu.in https://admissions.pydah.edu.in https://*.wix.com https://*.wixsite.com https://*.filesusr.com https://*.wix-code.com https://*.wixapps.net;",
-          },
-        ],
-      },
+      // {
+      //   source: '/:path*',
+      //   headers: [
+      //     {
+      //       key: 'Content-Security-Policy',
+      //       value: "frame-ancestors 'self' https://pydah.edu.in https://*.pydah.edu.in https://admissions.pydah.edu.in https://*.wix.com https://*.wixsite.com https://*.filesusr.com https://*.wix-code.com https://*.wixapps.net;",
+      //     },
+      //   ],
+      // },
     ];
   },
 };
